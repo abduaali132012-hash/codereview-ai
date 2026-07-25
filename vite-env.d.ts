96be1a677919da2944ab2efb21216fc766602e49
abduaali132012-hash <abduaali132012@gmail.com>
2026-07-25 18:29:08 +0300

    Refactor SVG module exports in vite-env.d.ts
    Refactor SVG module declarations to export ReactComponent directly.

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -3,14 +3,12 @@
 
 declare module '*.svg?react' {
   import * as React from 'react';
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
 }
 
 declare module '*.svg?import&react' {
   import * as React from 'react';
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
 }
